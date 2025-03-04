2ddc18cf05813eab63b37d5e67eb742c25832321
Jluza <Jluza21@gmail.com>
2025-03-04 17:18:50 +0900

    added docstrings

--- TextInputBox.py
+++ TextInputBox.py
@@ -1,16 +1,20 @@
 import pygame
 class TextInputBox:
     def __init__(self, rect, bg_color, input_active):
+        ''' constructor for text input objects '''
         self.rect = rect
         self.bg_color = bg_color
         self.input_active = input_active
     
     def draw(self, screen, text_surface, text_rect):
+        ''' attaches renders text to rectangle '''
         pygame.draw.rect(screen, self.bg_color, self.rect) # draw rectangle
         screen.blit(text_surface, text_rect) # Renders text and attaches to the rectangle
 
     def set_active(self, input_active):
+        ''' sets the the input boolean to check if the text box was selected '''
         self.input_active = input_active
 
     def get_active(self):
+        ''' return if the input box is selected of not '''
         return self.input_active
