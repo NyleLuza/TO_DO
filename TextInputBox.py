d258a7ddd59df449cdbce81d08b36a4e0de7489d
Jluza <Jluza21@gmail.com>
2025-03-04 14:45:16 +0900

    Created a new class for text input boxes to differentiate them from regular buttons, only the constructor has been made

---
+++ TextInputBox.py
@@ -0,0 +1,6 @@
+import pygame
+class TextInputBox:
+    def __init__(self, rect, bg_color, input_active):
+        self.rect = rect
+        self.bg_color = bg_color
+        self.input_active = input_active
